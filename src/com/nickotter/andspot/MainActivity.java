72def0fdae68345960bb6554837bf64a341c663d
otternq <otternq@gmail.com>
2014-02-12 15:51:34 -0800

    Adding a settings menu item through a menu.

--- src/com/nickotter/andspot/MainActivity.java
+++ src/com/nickotter/andspot/MainActivity.java
@@ -2,6 +2,8 @@ package com.nickotter.andspot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +13,13 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
